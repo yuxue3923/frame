fd74c5c1caf159df23b5308fa4238b6cc34f28e7
southproject <yukipedia_yui@yahoo.com>
2019-05-21 18:04:43 +0800

    5/21布局初始化

--- src/App.js
+++ src/App.js
@@ -1,28 +1,109 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Button,} from 'antd';
+import {  Layout, Menu, Icon,Input,Button,Card,Row,Col} from 'antd';
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux';
-class AppIndex extends Component {
-  
+const { Header, Sider, Content ,Footer} = Layout;
+const {TextArea} = Input;
+const ButtonGroup = Button.Group;
+export default class SiderDemo extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     return (
-      <div className="App">
-          <div style={{fontSize:25,}}>这是主页面</div>
+      <Layout style={{width: '100%', height: '100vh'}}>
+        <Sider trigger={null} collapsible collapsed={this.state.collapsed} collapsedWidth={0} breakpoint="xxl" defaultCollapsed width={249}
+          style={{
+            //  minHeight:893,
+            //  minWidth:2490
+            background:'rgb(249,249,249)',
+            minHeight:"100%",
+            borderRight:0
+             /* 200 115 */
+          }}
+        >
          
-         <Link to='/app/pagefir'><Button type="primary">1.1</Button></Link>
-         <Link to='/app/pagesec'><Button type="primary">1.2</Button></Link>
-         <Link to='/app/pagethir'><Button type="primary">1.3</Button></Link>
-         <Link to='/app/pagefour'><Button type="primary">1.4</Button></Link>
-         <div>
-            {this.props.children}
-        </div>
-      </div>
+         
+        <div className="logo">
+         </div>
+          
+        <Card style={{width:200,height:115,margin: "21px"}} className="single-viewer">
+        </Card>
+            
+        </Sider>
+        <Layout>
+          <Header style={{ background: '#fff', padding:0, }}>
+          <Row gutter={8}>
+          <Col span={6}>
+          <Icon type="appstore" />
+            <div> aaa </div>
+          </Col>
+          <Col span={6}>
+          <Icon type="appstore" />
+            <div> aaa </div>
+          </Col>
+          <Col span={6}>
+          <Icon type="appstore" />
+            <div> aaa </div>
+          </Col>
+          </Row>
+          {/* <div style={{display:"flex",flexDirection:"row",padding:0,height:"100%"}}>
+          <div type="primary" style={{height:"100%",display:"flex",flexDirection:"column",justifyContent:"center"}}>
+            <Icon type="appstore" style={{alignSelf:"center"}}/>
+               <div> aaa </div>
+          </div>
+          <Button type="primary" style={{height:"100%" ,margin:"",display:"flex",flexDirection:"column",justifyContent:"center"}}>
+            <Icon type="appstore" style={{alignSelf:"center"}}/>
+               <div> aaa </div>
+          </Button>
+          <Button type="primary" style={{height:"100%" ,margin:"",display:"flex",flexDirection:"column",justifyContent:"center"}}>
+            <Icon type="appstore" style={{alignSelf:"center"}}/>
+               <div> aaa </div>
+          </Button>
+        </div> */}
+       
+          </Header>
+          
+          <Layout style={{padding: ' 24px',}}>
+          <Content
+            style={{
+            //  margin: '24px 16px',
+              padding:24,
+              background: '#fff',
+              width: '100%', height: '100vh',
+    
+            }}
+          >
+        
+            Content
+         
+          </Content>
+          </Layout>
+          <Footer style={{ background: '#fff', padding: 10 }}>
+          <Icon
+             // className="trigger"
+              type={this.state.collapsed ? 'menu-unfold' : 'menu-fold'}
+              onClick={this.toggle}
+            />
+            <TextArea row ={5}/>
+          </Footer>
+        </Layout>
+      </Layout>
     );
   }
 }
 
-function mapStateToProps(state){
+
+
+/* function mapStateToProps(state){
   return{
      logininfo:state.reducerlogin.logininfo,
   };
@@ -36,3 +117,7 @@ export default connect(
   mapStateToProps,
   mapDispatchToProps,
 )(AppIndex);
+ */
+
+
+
